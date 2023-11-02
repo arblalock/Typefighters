a925d7e842f52901155dc640816c8a67d86ee7ea
Adam <adamrblalock@gmail.com>
2023-11-02 11:48:26 -0700

    wip

--- app/src/app/FriendCode/page.tsx
+++ app/src/app/FriendCode/page.tsx
@@ -5,14 +5,13 @@ export default function Page() {
 
     return (
         <CenterContainer>
-            <div className="m-6">
+            <div className="m-2">
                 <Button text="Create Game" textSize="3xl" buttonStyle="primary" />
             </div>
-            <div>------OR-------</div>
-            <div className="m-6">
-                <div className="">Enter Room Code:</div>
-                <div>
-                    <input></input>
+            <div className="text-base">------ OR ------</div>
+            <div className="m-2">
+                <div className="m-10">
+                    <input className="dark-input max-w-[280px] p-6 input-sm-pl text-4xl text-center" placeholder="Enter Room Code"></input>
                 </div>
             </div>
         </CenterContainer>
