79f195b9356c3ec5319c0b685c20482cfed80766
Adam <adamrblalock@gmail.com>
2023-11-06 12:13:04 -0800

    working on sharable link

--- app/src/app/FriendCode/page.tsx
+++ app/src/app/FriendCode/page.tsx
@@ -1,18 +1,16 @@
 import { Button } from "@/components/Button";
 import { CenterContainer } from "@/components/CenterContainer";
+import { ShareableLink } from "@/components/ShareableLink";
 
 export default function Page() {
 
     return (
         <CenterContainer>
-            <div className="m-2">
-                <Button text="Create Game" textSize="3xl" buttonStyle="primary" />
+            <div className="m-2 text-4xl">
+                Give your friend this link:
             </div>
-            <div className="text-base">------ OR ------</div>
             <div className="m-2">
-                <div className="m-10">
-                    <input className="dark-input max-w-[280px] p-6 input-sm-pl text-4xl text-center" placeholder="Enter Room Code"></input>
-                </div>
+                <ShareableLink text="testingalink.com" />
             </div>
         </CenterContainer>
     )
---
+++ app/src/components/ShareableLink.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { EleSize } from "@/types/comp-types";
+import { getTextSize } from "@/utils/css-utils";
+import { useState, useRef } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+import { link } from "fs/promises";
+
+export const ShareableLink = ({ text: linkTxt, cb, textSize = 'xl' }: { text: string, cb?: Function, textSize?: EleSize }) => {
+
+    const [mouseIsHovering, setMouseIsHovering] = useState<boolean>(false);
+    const [linkHoverClass, setLinkHoverClass] = useState<string>('')
+    const [linkClickedClass, setLinkClickedClass] = useState<string>('')
+    const [linkDefaultClass, setLinkDefaultClass] = useState<string>('sh-link-default')
+    const [linkIsClicked, setLinkIsClicked] = useState<boolean>(false);
+    const [descTxt, setDescTxt] = useState<string>(linkTxt)
+    const [linkAnimating, setLinkAnimating] = useState<Boolean>(false);
+    const [linkWidth, setLinkWidth] = useState<Number>(0);
+    const linkRef = useRef<any>(null);
+
+
+    const handleOnMouseEnter = () => {
+        setLinkWidth(linkRef.current ? linkRef.current.offsetWidth : null);
+        setMouseIsHovering(true);
+        if (linkAnimating == false && linkIsClicked == false) {
+            setLinkHoverClass('sh-link-hover')
+        } else {
+            setLinkHoverClass('')
+        }
+    }
+
+    const handleOnMouseLeave = () => {
+        setMouseIsHovering(false);
+        setLinkHoverClass('');
+    }
+
+    const handleLinkCopyClick = (isIconClick: Boolean) => {
+        if (cb) {
+            cb();
+        }
+        if (linkIsClicked && isIconClick == false) return;
+        setLinkIsClicked(true);
+        setDescTxt('Link copied!')
+        setLinkAnimating(true);
+        setLinkHoverClass('');
+        setLinkClickedClass('sh-link-clicked');
+        //Need to set width so that changing text does not modify width
+        linkRef.current.style.width = `${linkWidth}px`
+        setTimeout(() => resetLink(), 3000)
+    }
+
+    const resetLink = () => {
+        setLinkDefaultClass('');
+        setDescTxt(linkTxt);
+        setLinkAnimating(false);
+        setLinkClickedClass('');
+    }
+
+    return (
+        <div ref={linkRef} className={`relative sh-link m-6 px-24 py-6 ${linkDefaultClass} ${linkHoverClass} ${linkClickedClass}`}
+            style={{
+                fontSize: getTextSize(textSize)
+            }}
+            onMouseEnter={() => handleOnMouseEnter()}
+            onMouseLeave={() => handleOnMouseLeave()}
+            onClick={() => handleLinkCopyClick(false)}>
+            {(linkIsClicked || mouseIsHovering) &&
+                <div
+                    onClick={() => handleLinkCopyClick(true)}
+                    className={`absolute px-[10%] valign left-0 ${linkAnimating ? '' : 'icon-btn-dark'}`}>
+                    <FontAwesomeIcon icon={faCopy} />
+                </div>}
+            {<span>{descTxt}</span>}
+        </div>
+    )
+}
